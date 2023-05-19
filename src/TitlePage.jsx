65aace0f191569185cb82fa3ef5391fcffb64db0
Eugene <eugene_doris@hotmail.com>
2023-05-19 16:42:09 +0100

    more change to title size

--- src/TitlePage.jsx
+++ src/TitlePage.jsx
@@ -12,7 +12,6 @@ return (
     <h1 className = 'titleText'>CRYPTIC BOND</h1>
     <div className = 'description'><span className="spanBackground">Figure out which James Bond movie the following clues, quotes and descriptions relate to. There are 10 rounds.  Think long and hard about what each clue may relate to and what Bond movie you think of.  
     Best of luck, Bond fans!</span></div>
-    <br></br>
 
 <div className = 'start'><button className = 'startButton' onClick={() => props.comMence()}>Start the Game</button></div>
 
