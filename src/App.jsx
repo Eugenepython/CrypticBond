2df68ea404020ed3c4064526914298caf34e0965
Eugene <eugene_doris@hotmail.com>
2023-05-19 17:09:56 +0100

    playing with image

--- src/App.jsx
+++ src/App.jsx
@@ -55,13 +55,13 @@ useEffect(() => {
 //console.log("the score is "  + count)
 //console.log("the round is " + round)
 
-
+//`url(${image})`
 
   return (
     <>
       <div>
         <TitlePage comMence = {beGin} showTitle = {showTitle} />
-        <div className='mystery' style={{ backgroundImage: `url(${image})`, display: showTitle ? 'none' : 'block' }}>
+        <div className='mystery' style={{ backgroundImage: `url("/images/7.jpg")`, display: showTitle ? 'none' : 'block' }}>
         {round <= numberOfQuestions ? <div className = 'rounds'> <h2>Round {round}</h2> </div>:null}
     <div>{round <= (numberOfQuestions ) ?<SortData clickItInSortData = {clickItInApp} selectedButton = {selectedButton} /> :null}</div>
     {round <= numberOfQuestions ? <div className = 'currentScore'> <h3>Score : {count}</h3> </div>:null}
