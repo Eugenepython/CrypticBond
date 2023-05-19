72ce895d5ba6181834b8371401d53be5dad085a8
Eugene <eugene_doris@hotmail.com>
2023-05-19 09:40:27 +0100

    changing config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base : "/CrypticBond",
   resolve: {
     alias: {
       '/@images/': path.resolve(__dirname, './src/images'),
