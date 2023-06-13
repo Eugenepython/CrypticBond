7f841f921fd05b9ec88cf985277cc1251f950bde
Eugenepython <50318257+Eugenepython@users.noreply.github.com>
2023-06-13 01:12:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base : "/CrypticBond",
+  base : "/",
   resolve: {
     alias: {
       '/@images/': path.resolve(__dirname, './src/images'),
